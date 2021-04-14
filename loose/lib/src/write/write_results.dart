970687c10029168df6dde0f6fd3778c8e386cf7f
Kirk Linkletter <kirklinkletter@gmail.com>
2021-04-14 19:33:13 +0000

    change write_results default time

--- loose/lib/src/write/write_results.dart
+++ loose/lib/src/write/write_results.dart
@@ -6,8 +6,8 @@ class WriteResult {
   final Writable write;
 
   String get label => write.label;
-  DateTime get updateTime => DateTime.tryParse(_updateTime) ?? DateTime.utc(0);
-  bool get ok => updateTime != DateTime.utc(0);
+  DateTime get updateTime => DateTime.tryParse(_updateTime) ?? DateTime.utc(1);
+  bool get ok => updateTime != DateTime.utc(1);
 
   const WriteResult(this._updateTime, this.status, this.write);
 }
